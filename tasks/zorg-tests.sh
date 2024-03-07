10901c6c9e285b485c7119831bf75698f8753ffb
Justice Adams <jadams23@apple.com>
2024-03-07 12:04:55 -0800

    fix zorg-tests, no need for an svn clone

--- tasks/zorg-tests.sh
+++ tasks/zorg-tests.sh
@@ -1,8 +1,6 @@
-build get zorg
-
 . "${TASKDIR}"/utils/venv.sh
 . "${TASKDIR}"/utils/venv_lit.sh
 
 mkdir -p result
-cd "zorg/test/jenkins"
+cd "config/test/jenkins"
 lit --xunit-xml-output="${WORKSPACE}/result/xunit.xml" -v .
